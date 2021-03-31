45c6d8bb0ca7046629fa3bf37cd53d5ef2498e34
Eva.Wang <elphinhote@yahoo.com>
2021-03-30 20:21:55 -0700

    firts commit

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+
+// connection file
+
+const mysql = require("mysql")
+let connection
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
+
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "omnicode",
+    database: "burgers_db",
+});
+};
+
+connection.connect(function(err) {
+    if (err) throw (err);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
